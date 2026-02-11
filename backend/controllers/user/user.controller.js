28d601b175373aa7af214e09be469c654034c2c1
elprofessor67 <jeeshanr599@gmail.com>
2026-02-11 23:49:36 +0530

    fixed

--- backend/controllers/user/user.controller.js
+++ backend/controllers/user/user.controller.js
@@ -22,7 +22,7 @@ const hashOtp = (userId, otp) => {
 };
 
 const sendOtpEmailViaMailer = async ({ email, otp }) => {
-  const resp = await fetch("https://mailing.hgcradio.com/send-email", {
+  const resp = await fetch("https://mailing.hgcradio.org/send-email", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
